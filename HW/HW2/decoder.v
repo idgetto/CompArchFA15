973e645a23a9ddd20faaa7e4c15d6ec4acb8889b
Isaac Getto <isaac.getto@gmail.com>
2015-09-20 17:45:47 -0400

    implement 2-bit decoder

--- HW/HW2/decoder.v
+++ HW/HW2/decoder.v
@@ -1,3 +1,11 @@
+// add delay to gates
+`define AND and #50
+`define OR or #50
+`define XOR xor #50
+`define NOT not #50
+`define NAND nand #50
+`define NOR nor #50
+
 module behavioralDecoder(out0,out1,out2,out3, address0,address1, enable);
 output out0, out1, out2, out3;
 input address0, address1;
@@ -9,15 +17,43 @@ module structuralDecoder(out0,out1,out2,out3, address0,address1, enable);
 output out0, out1, out2, out3;
 input address0, address1;
 input enable;
-  // Your decoder code here
+
+// output zero
+// ~(add0 + add1) * enable
+wire out0_val;
+`NOR(out0_val, address0, address1);
+`AND(out0, out0_val, enable);
+
+// output one
+// add0 * ~add1 * enable
+wire n_add1;
+wire out1_val;
+`NOT(n_add1, address1);
+`AND(out1_val, address0, n_add1);
+`AND(out1, out1_val, enable);
+
+// output two
+// ~add0 * add1 * enable
+wire n_add0;
+wire out2_val;
+`NOT(n_add0, address0);
+`AND(out2_val, address1, n_add0);
+`AND(out2, out2_val, enable);
+
+// output three
+// add0 * add1 * enable
+wire out3_val;
+`AND(out3_val, address0, address1);
+`AND(out3, out3_val, enable);
+
 endmodule
 
 module testDecoder; 
 reg addr0, addr1;
 reg enable;
 wire out0,out1,out2,out3;
-behavioralDecoder decoder (out0,out1,out2,out3,addr0,addr1,enable);
-//structuralDecoder decoder (out0,out1,out2,out3,addr0,addr1,enable); // Swap after testing
+//behavioralDecoder decoder (out0,out1,out2,out3,addr0,addr1,enable);
+structuralDecoder decoder (out0,out1,out2,out3,addr0,addr1,enable); // Swap after testing
 
 initial begin
 $display("En A0 A1| O0 O1 O2 O3 | Expected Output");
